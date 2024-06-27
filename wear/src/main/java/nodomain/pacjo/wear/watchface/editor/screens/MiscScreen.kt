8d37a9e359e1bf44db6233179cfb83c0a8324e93
pacjo <kamil.kroliczek2004@gmail.com>
2024-06-27 13:24:56 +0200

    (main): fix item alignment on round watches in MiscScreen

--- wear/src/main/java/nodomain/pacjo/wear/watchface/editor/screens/MiscScreen.kt
+++ wear/src/main/java/nodomain/pacjo/wear/watchface/editor/screens/MiscScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.wear.compose.foundation.lazy.AutoCenteringParams
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.foundation.lazy.items
 import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
@@ -18,16 +19,17 @@ import androidx.wear.compose.material.ToggleChip
 
 @Composable
 fun MiscConfigScreen(settings: List<@Composable () -> Unit>) {
-    val listState = rememberScalingLazyListState()
+    val listState = rememberScalingLazyListState(0)
 
-    Scaffold (
+    Scaffold(
         positionIndicator = {
             PositionIndicator(scalingLazyListState = listState)
         }
     ) {
         ScalingLazyColumn(
             modifier = Modifier.fillMaxSize(),
-            state = listState
+            state = listState,
+            autoCentering = AutoCenteringParams(0, 1)
         ) {
             items(settings) { setting ->
                 setting()
