8217cab46fe3ee4365131eab1221017f2b716bb7
pacjo <kamil.kroliczek2004@gmail.com>
2025-09-05 21:38:27 +0200

    (:jetpack): get over the empty WatchFaceFeature interface

--- wear/jetpack/watchfaces/base/src/main/java/nodomain/pacjo/wear/watchface/base/feature/WatchFaceFeature.kt
+++ wear/jetpack/watchfaces/base/src/main/java/nodomain/pacjo/wear/watchface/base/feature/WatchFaceFeature.kt
@@ -1,7 +1,6 @@
 package nodomain.pacjo.wear.watchface.base.feature
 
-// TODO: figure out if we want anything here
 /**
- * The fundamental contract for any feature that can be added to a watch face.
+ * The fundamental interface for any feature that can be added to a watch face.
  */
 interface WatchFaceFeature
